2a1d0079297a7de75d6027aad6252c13131c11c9
Nguyen Thanh Duoc <Nguyen Thanh Duoc@DUOCNT>
2020-03-15 22:54:48 +0700

    publish project

---
+++ BootstrapAlertInMVC/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapAlertInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int type)
+        {
+            string message = string.Empty;
+            if (type == 1)
+            {
+                message = "sucsess message";
+            }
+            else if (type == 2)
+            {
+                message = "warning message";
+            }
+            else if (type == 3)
+            {
+                message = "danger message";
+            }
+            else
+            {
+                message = "Nothing";
+            }
+
+            SetAlert(message, type);
+
+            return View();
+        }
+
+        protected void SetAlert(string message, int type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == 1)
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == 2)
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == 3)
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+            else
+            {
+                TempData["AlertType"] = "alert-info";
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
